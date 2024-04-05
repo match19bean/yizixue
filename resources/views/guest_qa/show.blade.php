a14b5ece2a402546db1ec2d04a6b07666b5ddb60
toshow <kidtoshow@nextec.com.tw>
2024-04-05 10:03:12 +0800

    整合前端

--- resources/views/guest_qa/show.blade.php
+++ resources/views/guest_qa/show.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.guest2')
 
 @section('content')
-    <div class="row text-gray-600">
+<div class="question-and-answer-page">
+<div class="row text-gray-600">
         <h4 class="mt-3">首頁 > 問與答</h4>
     </div>
-
-    <div class="row mt-4">
+    <div class="row mt-4 qaSection">
+        <!-- main -->
         <div class="col-8">
+            <!-- Basic informations -->
             <div class="row">
-                <div class="col-2">
-                    <span class="w-100"><i class="fa fa-circle" style="font-size: 5rem; color: #6D757D;"></i></span>
-                </div>
                 <div class="col-10">
+                    <!-- date section -->
                     <p>{{$qna->created_at->format('Y/m/d')}}</p>
+                    <!-- The question title -->
                     <h2>{{$qna->title}}</h2>
+                    <!-- QA tags -->
                     <div class="row row-cols-4">
                         @if(!is_null($qna->categoryRelation))
                             @foreach($qna->categoryRelation as $relation)
@@ -27,69 +29,103 @@
                     </div>
                 </div>
             </div>
-            <div class="row mt-3 border-bottom-secondary d-flex">
+            <!-- details -->
+            <div class="row mt-3 border-b-line d-flex">
                 <span for="" class="btn text-white w-25 mb-1" style="background-color: #4C2A70">金額</span>
-                <p class="w-75 align-bottom pt-2 text-center">{{$qna->amount_down}}-{{$qna->amount_up}}</p>
+                <p class="w-75 align-bottom pt-2">{{$qna->amount_down}}-{{$qna->amount_up}}</p>
             </div>
-            <div class="row mt-3 border-bottom-secondary d-flex">
+            <div class="row mt-3 border-b-line d-flex">
                 <span for="" class="btn text-white w-25 mb-1" style="background-color: #4C2A70">時間</span>
-                <p class="w-75 align-bottom pt-2 text-center">{{ Carbon\Carbon::parse($qna->contact_time)->format('Y-m-d H:i:s')}}  至 {{ \Carbon\Carbon::parse($qna->contact_time_end)->format('Y-m-d H:i:s')}}</p>
+                <p class="w-75 align-bottom pt-2">{{ Carbon\Carbon::parse($qna->contact_time)->format('Y-m-d H:i:s')}}  至 {{ \Carbon\Carbon::parse($qna->contact_time_end)->format('Y-m-d H:i:s')}}</p>
             </div>
-            <div class="row mt-3 border-bottom-secondary d-flex">
+            <div class="row mt-3 border-b-line d-flex">
                 <span for="" class="btn text-white w-25 mb-1" style="background-color: #4C2A70">地點</span>
-                <p class="w-75 align-bottom pt-2 text-center">{{$qna->place}}</p>
+                <p class="w-75 align-bottom pt-2">{{$qna->place}}</p>
             </div>
-            <div class="row mt-3 border-bottom-secondary d-flex">
+            <div class="row mt-3 border-b-line d-flex">
                 <span for="" class="btn text-white w-25 mb-1" style="background-color: #4C2A70">說明</span>
-                <p class="w-75 align-bottom pt-2 text-center text-break">
+                <p class="w-75 align-bottom pt-2 text-break">
                     {!! $qna->body !!}
                 </p>
             </div>
+            <div class="row mt-3 border-b-line d-flex">
+                <span class="btn text-white w-25 mb-1" style="background-color: #4C2A70">參考附件</span>
+                <!-- attachments -->
+                <div class="attachment d-flex flex-row">
+                    <!-- files -->
+                    <div class="file d-flex">
+                        <svg>
+                            <rect width="100%" height="100%" x="0" y="0" fill="white" />
+                        </svg>
+                    </div>
+                    <div class="file d-flex">
+                        <svg>
+                            <rect width="100%" height="100%" x="0" y="0" fill="white" />
+                        </svg>
+                    </div>
+                    <div class="file d-flex">
+                        <svg>
+                            <rect width="100%" height="100%" x="0" y="0" fill="white" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- side info -->
         <div class="col-4">
-
             <div class="card">
                 <div class="card-header text-white text-center" style="background-color: #4C2A70">
                     <h3>聯絡資訊</h3>
                 </div>
                 @if(auth()->check() &&  auth()->user()->expired >= now() )
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-3">
+                    <!-- name card -->
+                    <div class="avatarName mb-3">
+                        <div class="avatar">
                             @if(!is_null($qna->author->avatar))
                                 <img class="w-100" src="{{asset('uploads/'.$qna->author->avatar)}}" alt="user avatar">
                             @else
                                 <img class="w-100" src="{{asset('uploads/images/default_avatar.png')}}" alt="user avatar">
                             @endif
                         </div>
-                        <div class="col-9 text-white text-center" style="background-color: #BD9EBE">
+                        <div class="nameCard text-white" style="background-color: #BD9EBE">
                             <span style="font-size: 1.5rem;">{{$qna->nickname}}</span>
                         </div>
                     </div>
-                    <div class="row my-3">
+                    <!-- contact -->
+                    <div class="row contact mb-3">
                         <p class="px-5">
                             聯絡電話
                         </p>
-                        <p class="px-5 py-2 btn mx-5 w-75 text-center text-white" style="background-color: #BD9EBE">
-                            {{$qna->phone}}
-                        </p>
+                        <svg class="px-3 mx-3" viewBox="0 0 675 126">
+                            <rect x="45" width="630" height="126" rx="20" ry="20" fill="#BD9EBE"/>
+                            <path d="M60.5,18.5c0,0-41,8-60-19c0,0,11,47.2,60,47.2" fill="#BD9EBE"/>
+                            <text x="30%" y="60%" fill="#ffffff">{{$qna->phone}}</text>
+                        </svg>
                     </div>
-                    <div class="row my-3">
+                    <div class="row contact mb-3">
                         <p class="px-5">
                             Email
                         </p>
-                        <p class="px-5 py-2 btn mx-5 w-75 text-center text-white" style="background-color: #BD9EBE">
-                            {{$qna->email}}
-                        </p>
+                        <svg class="px-3 mx-3" viewBox="0 0 675 126">
+                            <rect x="45" width="630" height="126" rx="20" ry="20" fill="#BD9EBE"/>
+                            <path d="M60.5,18.5c0,0-41,8-60-19c0,0,11,47.2,60,47.2" fill="#BD9EBE"/>
+                            <text x="30%" y="60%" fill="#ffffff">{{$qna->email}}</text>
+                        </svg>
                     </div>
-                    <div class="row my-3">
+                    <div class="row contact mb-3">
                         <p class="px-5">
                             Line
                         </p>
-                        <p class="px-5 py-2 btn mx-5 w-75 text-center text-white" style="background-color: #BD9EBE">
-                            {{$qna->line}}
-                        </p>
+                        <svg class="px-3 mx-3" viewBox="0 0 675 126">
+                            <rect x="45" width="630" height="126" rx="20" ry="20" fill="#BD9EBE"/>
+                            <path d="M60.5,18.5c0,0-41,8-60-19c0,0,11,47.2,60,47.2" fill="#BD9EBE"/>
+                            <text x="30%" y="60%" fill="#ffffff">{{$qna->line}}</text>
+                        </svg>
                     </div>
+                </div>
+                <div class="card-footer text-white text-center" style="background-color: #4C2A70">
+                    <a href="{{route('pay-product-list')}}" class="text-decoration-none text-white"><h4>點擊查看</h4></a>
                 </div>
                 @endif
                 @if(auth()->guest() || auth()->user()->expired < now())
@@ -100,35 +136,39 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="row">
+    <div class="row similarQ">
+        <div class="row m-3">
             <h2 style="color: #4C2A70;">相似問題</h2>
         </div>
         <div class="row">
             @if(!is_null($sameqna))
                 @foreach($sameqna as $qa)
-                    <div class="col-6 row py-1">
+                    <div class="col-5 row py-1 m-3">
                         <div class="col-11 text-lg p-3" style="border-left-style: solid; border-top-style: solid; border-bottom-style: solid; border-color: #6D757D;">
-                            <h2 class="d-flex">
-                                <i class="fa fa-circle px-3" style="color:gray"></i>
+                            <div class="titleInfo">
+                                <i class="fa fa-circle" style="color:gray"></i>
                                 <a href="{{route('qna.show', $qa->qa_id)}}" class="text-decoration-none text-black-50">{{$qa->qa->title}}</a>
-                                <span class="w-100 text-right">
+                                <span class="w-100">
                                     @if($qa->category)
-                                        <span class="btn px-4 text-center text-white" style="background-color: #4C2A70">
+                                        <span class="btn text-center text-white" style="background-color: #4C2A70">
                                             {{$qa->category->name}}
                                         </span>
                                     @endif
                                 </span>
-                            </h2>
-                            <div class="row text-center text-break">
+                            </div>
+                            <div class="row text-break">
                                 {!! \Illuminate\Support\Str::limit($qa->qa->body) !!}
                             </div>
                         </div>
-                        <div class="col-1" style="background-color: #4C2A70"></div>
+                        <div class="qStyle col">
+                            <svg viewBox="0 0 45 150">
+                                <polygon fill="#4C2A70" points="50,150 0,150 35,75 0,0 50,0 "/>
+                            </svg>
+                        </div>
                     </div>
-
                 @endforeach
             @endif
         </div>
     </div>
+</div>
 @endsection
