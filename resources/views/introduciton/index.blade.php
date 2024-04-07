f300b97f1ec8ee55d3692be2c94c9948db040614
toshow <kidtoshow@nextec.com.tw>
2024-04-08 00:47:00 +0800

    修改學校名稱為英文

--- resources/views/introduciton/index.blade.php
+++ resources/views/introduciton/index.blade.php
@@ -283,7 +283,7 @@
                                     <!-- name card -->
                                     <div class="name-card">
                                         <h4>{{ $user->name }}</h4>
-                                        <h4>{{ !is_null($user->universityItem) ? $user->universityItem->english_name:'' }}</h4>
+                                        <h6>{{ !is_null($user->universityItem) ? $user->universityItem->english_name:'' }}</h6>
                                     </div>
                                     <!-- post tag -->
                                     <div class="postTags">
