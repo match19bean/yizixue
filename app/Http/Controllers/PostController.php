c93fd872a4739e658c987e3a963f0251f8ab0cee
toshow <kidtoshow@nextec.com.tw>
2024-04-07 22:35:38 +0800

    修改發表完回列表

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -98,7 +98,7 @@ class PostController extends Controller
             }
         }
 
-        return url('list-posts');
+        return redirect()->url('list-posts');
     }
 
     public function edit($uuid)
