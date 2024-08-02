c68ef494fa61aa2a19abe208366b7682e2ce84c9
toshow <kidtoshow@nextec.com.tw>
2024-08-02 12:17:28 +0800

    修改輪播圖

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="l-innerHeader__headerCard">
+                    <input type="hidden" value="{{asset('uploads/'.$Data['Carousel']->image_path)}}">
                     <div class="l-innerHeader__headerCard__banner">
                         <span id="bannerImg"
                             style="background-image: {{ !empty($Data['Carousel']) ? asset('uploads/'.$Data['Carousel']->image_path) : '' }}">&nbsp;</span>
