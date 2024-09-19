38f2b20f49b057bfdccf79d9197925e7709cc217
toshow <kidtoshow@nextec.com.tw>
2024-09-19 19:34:14 +0800

    fb_test

--- app/Http/Controllers/Auth/FacebookController.php
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -25,31 +25,31 @@ class FacebookController extends Controller
     {
         logger('request');
         logger($request);
-        $code = $request->input('code');
-        logger('code');
-        // 用 code 交換 access token
-        $client = new Client();
-//        $response = $client->request('GET', 'https://graph.facebook.com/v2.12/oauth/access_token', [
-        $response = $client->request('GET', 'https://graph.facebook.com/v19.0/oauth/access_token', [
-            'query' => [
-                'client_id' => $this->clientId,
-                'redirect_uri' => $this->redirectUri,
-                'client_secret' => $this->clientSecret,
-                'code' => $code,
-            ]
-        ]);
-        logger('response');
-        logger($response);
-        $data = json_decode($response->getBody(), true);
-        logger('data');
-        logger('data');
-        $accessToken = $data['access_token'];
-
-        // 使用 access token 獲取用戶資料
-        $userResponse = $client->request('GET', 'https://graph.facebook.com/me?fields=id,name,email&access_token=' . $accessToken);
-        $user = json_decode($userResponse->getBody(), true);
-        logger('user');
-        logger($user);
+//        $code = $request->input('code');
+//        logger('code');
+//        // 用 code 交換 access token
+//        $client = new Client();
+////        $response = $client->request('GET', 'https://graph.facebook.com/v2.12/oauth/access_token', [
+//        $response = $client->request('GET', 'https://graph.facebook.com/v19.0/oauth/access_token', [
+//            'query' => [
+//                'client_id' => $this->clientId,
+//                'redirect_uri' => $this->redirectUri,
+//                'client_secret' => $this->clientSecret,
+//                'code' => $code,
+//            ]
+//        ]);
+//        logger('response');
+//        logger($response);
+//        $data = json_decode($response->getBody(), true);
+//        logger('data');
+//        logger('data');
+//        $accessToken = $data['access_token'];
+//
+//        // 使用 access token 獲取用戶資料
+//        $userResponse = $client->request('GET', 'https://graph.facebook.com/me?fields=id,name,email&access_token=' . $accessToken);
+//        $user = json_decode($userResponse->getBody(), true);
+//        logger('user');
+//        logger($user);
 
         // 這裡可以使用 $user 進行用戶的登入或註冊邏輯
         // 例如：User::firstOrCreate([...]);
