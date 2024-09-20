3d6caa649c718adc9b23aea14d7d1ab495d77dd9
toshow <kidtoshow@nextec.com.tw>
2024-09-20 11:58:06 +0800

    fb delete api

--- app/Http/Controllers/Auth/FacebookController.php
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -89,6 +89,38 @@ class FacebookController extends Controller
     }
 
     public function delete(Request $request){
-        logger($request->all());
+        $signed_request = $request->signed_request;
+        $data = $this->parse_signed_request($signed_request);
+        $user_id = $data['user_id'];
+
+        if(!empty($user_id)){
+            $user = User::where('fb_auth', $user_id)->first();
+            if(!empty($user)){
+                $user->fb_auth = null;
+                $user->save();
+            }
+        }
+    }
+    function parse_signed_request($signed_request) {
+        list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+
+        $secret = $this->clientSecret; // Use your app secret here
+
+        // decode the data
+        $sig = $this->base64_url_decode($encoded_sig);
+        $data = json_decode($this->base64_url_decode($payload), true);
+
+        // confirm the signature
+        $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+        if ($sig !== $expected_sig) {
+            error_log('Bad Signed JSON signature!');
+            return null;
+        }
+
+        return $data;
+    }
+
+    function base64_url_decode($input) {
+        return base64_decode(strtr($input, '-_', '+/'));
     }
 }
