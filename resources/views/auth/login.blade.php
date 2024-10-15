657ace200ae483b43f662e16477942c89da0df4d
toshow <kidtoshow@nextec.com.tw>
2024-10-16 02:09:47 +0800

    整合前端

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,12 +67,14 @@
                 </button>
                 <div class="col-md-6">
                     <a href="{{url('line')}}" class="o-basicLink btn btn-user btn-block line">
-                        Login with Line
+                    <i class="bi bi-line o-socialIcon o-socialIcon__line"></i>
+                    <p>Login with Line</p>
                     </a>
                 </div>
                 <div class="col-md-6">
                     <a href="{{route('facebook-login')}}" class="o-basicLink btn btn-user btn-block line">
-                        Login with Meta
+                    <i class="bi bi-meta o-socialIcon o-socialIcon__meta"></i>
+                    <p>Login with Meta</p>
                     </a>
                 </div>
             </form>
